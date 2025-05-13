a411ac497e725d56c5cd2d77034b86be470db65e
kihec <hananmesic301@gmail.com>
2025-05-13 15:48:28 +0200

    add: tailwindcss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,17 @@
-import { useState } from 'react'
-import './App.css'
-
-import {Header} from './components/Header'
-import {Login} from './components/Login'
-import {Footer} from './components/Footer'
+import { Header } from "./components/Header";
+import { Login } from "./components/Login";
+import { Footer } from "./components/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-         <Header/>
-         <Login/>
-         <Footer/>
+        <Header />
+        <Login />
+        <Footer />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
