7e2721b5742737f38510d53d1a6a1fd9020814f5
gal badrov <gal.badrov@gmail.com>
2025-05-17 12:32:41 +0200

    fix: developers timetable

--- frontend/src/components/dashboard/DataDisplay/DevelopersTimeline.tsx
+++ frontend/src/components/dashboard/DataDisplay/DevelopersTimeline.tsx
@@ -41,24 +41,48 @@ export function DevelopersTimeline() {
     ],
   ];
 
+  const start = new Date(2023, 4, 16, 9, 0);
+  const end = new Date(2023, 4, 16, 17, 0);
+
+  const generateHourlyTicks = (start: Date, end: Date) => {
+    const ticks: Date[] = [];
+    let current = new Date(start);
+    while (current <= end) {
+      ticks.push(new Date(current));
+      current.setHours(current.getHours() + 1);
+    }
+    return ticks;
+  };
+
   const options = {
     timeline: {
       showRowLabels: true,
       groupByRowLabel: true,
+      rowLabelStyle: { color: "#F9FAFB", fontSize: 14 },
+      barLabelStyle: { color: "#F9FAFB", fontSize: 12 },
     },
-    avoidOverlappingGridLines: false,
+    hAxis: {
+      ticks: generateHourlyTicks(start, end),
+      textStyle: { color: "#F9FAFB", fontSize: 12 },
+      gridlines: { color: "#F9FAFB" },
+    },
+    backgroundColor: "#111827",
   };
 
   return (
-    <div>
-      <h2>Project Timetable</h2>
+    <div className='max-w-4xl bg-[#111827] rounded-lg shadow-md mt-4 p-6'>
+      <h2 className='text-2xl font-semibold mb-4 text-[#F9FAFB]'>
+        Project Timetable
+      </h2>
       <Chart
         chartType='Timeline'
         data={[columns, ...rows]}
         options={options}
         width='100%'
         height='400px'
-        loader={<div>Nalaganje grafa...</div>}
+        loader={
+          <div className='text-center text-gray-500'>Loading graph...</div>
+        }
       />
     </div>
   );
